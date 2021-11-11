408bd41b2ea9df23923a933a198c0a1d32b7de50
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-11-11 11:35:33 -0800

    integrated react router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import Header from '../elements/Header/Header';
 import Home from '../Home/Home';
+import Movie from '../Movie/Movie';
+import NotFound from '../elements/NotFound/NotFound';
 
 const App = () => {
     return (
-        <div>
+        <BrowserRouter>
+        <React.Fragment>
             <Header />
-            <Home />
-        </div>
+            <Switch>
+                <Route path="/" component={Home} exact />
+                <Route path="/:movieId" component={Movie} exact />
+                <Route component={NotFound} />
+            </Switch>
+        </React.Fragment>
+        </BrowserRouter>
     )
 }
 
