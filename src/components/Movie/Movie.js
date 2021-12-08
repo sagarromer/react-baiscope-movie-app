95b4c5ee56457eefd07acdf82e0a666cd4c49c1d
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-12-08 09:02:20 -0800

    fixed errors

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -27,7 +27,7 @@ class Movie extends Component {
         fetch(endpoint)
         .then(result => result.json())
         .then(result => {
-        console.log
+        
         if (result.status_code) {
             this.setState({ loading: false });
         } else {
